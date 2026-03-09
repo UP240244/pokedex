5f12bdfac2f1133c849c3fb512f26a358a89c965
UP240244 <up240244@alumnos.upa.edu.mx>
2026-03-09 15:23:12 -0600

    get pokemon data

--- app/pokemon/[name].tsx
+++ app/pokemon/[name].tsx
@@ -1,13 +1,28 @@
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text } from "react-native";
 
 export default function PokemonDetailsScreen() {
+  const [pokemonData, setPokemonData] = useState([]);
   const params = useLocalSearchParams();
 
+  useEffect(() => {
+    getPokemonData();
+  }, []);
+
+  const getPokemonData = async () => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${params.name}`;
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    const data = await response.json();
+    setPokemonData(data);
+  };
+
   return (
-    <View>
+    <ScrollView>
       <Text>{params.name}</Text>
-    </View>
+      <Text>{JSON.stringify(pokemonData)}</Text>
+    </ScrollView>
   );
 }
